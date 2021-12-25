6e8ad729195fc7c63961a31f304e0b9650721804
Tianhua Fang <tianhuaf0526@gmail.com>
2021-12-25 13:58:57 -0800

    reorg

---
+++ TensorflowTutorial/Keras/ImageClassification.py
@@ -0,0 +1,12 @@
+"""
+https://www.tensorflow.org/tutorials/keras/classification
+"""
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
